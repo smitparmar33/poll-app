863fea37f09939a1896d293d5451a689338a2c72
cyborg-at-811608745745 <cyborg-at-811608745745>
2021-12-29 14:16:30 +0530

    Revert "Hiding secret key"
    This reverts commit ebd44fb4ad0f126995afc9e9fb577bd385ce99d8.

--- pollme/settings.py
+++ pollme/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
-import environ
-load_dotenv()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'x*za6xf&_80ofdpae!yzq61g9ffikkx9$*iygbl$j7rr4wlf8t'
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = 'x*za6xf&_80ofdpae!yzq61g9ffikkx9$*iygbl$j7rr4wlf8t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
